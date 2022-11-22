523f489063ff41cc6af26f356a2649d002b49e59
ksh <seongs156@gmail.com>
2022-11-22 12:07:05 +0900

    변수 인라인하기

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 import {plays, invoices} from "./data.js";
 
-function statement(invoice, plays) {
+function statement(invoice) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
     const format = new Intl.NumberFormat("en-Us",{style:"currency",currency:"USD",minimumFractionDigits:2}).format;
 
     for (let perf of invoice.performances) {
-        const play = playFor(perf);
-        let thisAmount = amountFor(perf,play);
+
+        let thisAmount = amountFor(perf,playFor(perf));
         //포인트적립
         volumeCredits += Math.max(perf.audience - 30, 0);
         //희극 관객 5명마다 추가 포인트 제공
-        if("comedy" === play.type) {
+        if("comedy" === playFor(perf).type) {
             volumeCredits += Math.floor(perf.audience / 5);
         }
 
         //청구내역 출력
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
+        result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
         totalAmount += thisAmount;
     }
     result += `총액: ${format(totalAmount/100)}\n`;
