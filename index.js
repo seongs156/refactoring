0f17fcd5d492757b89ab4b5d81b0455ce045c636
ksh <seongs156@gmail.com>
2022-11-23 11:48:23 +0900

    format 함수선언 바꾸기

--- index.js
+++ index.js
@@ -4,17 +4,16 @@ function statement(invoice) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-    const format = new Intl.NumberFormat("en-Us",{style:"currency",currency:"USD",minimumFractionDigits:2}).format;
 
     for (let perf of invoice.performances) {
 
         volumeCredits += volumeCreditsFor(perf);
 
         //청구내역 출력
-        result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`;
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `총액: ${format(totalAmount/100)}\n`;
+    result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     console.log(result);
     return result;
@@ -24,13 +23,17 @@ function playFor(aPerformance) {
     return plays[aPerformance.playID];
 }
 
+function usd(aNumber) {
+   return new Intl.NumberFormat("en-Us",{style:"currency",currency:"USD",minimumFractionDigits:2}).format(aNumber/100);
+}
+
 function volumeCreditsFor(perf) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    let result = 0;
+    result += Math.max(perf.audience - 30, 0);
     if("comedy" === playFor(perf).type) {
-        volumeCredits += Math.floor(perf.audience / 5);
+        result += Math.floor(perf.audience / 5);
     }
-    return volumeCredits;
+    return result;
 }
 
 function amountFor(aPerformance) {
