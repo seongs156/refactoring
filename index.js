32eaf39aa28ed2d14e11be53a7082b27bddb3614
ksh <seongs156@gmail.com>
2022-11-23 10:50:59 +0900

    amountFor() 함수선언 바꾸기

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ function playFor(aPerformance) {
     return plays[aPerformance.playID];
 }
 
-function amountFor(aPerformance, play) {
+function amountFor(aPerformance) {
     let result = 0;
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
         case "tragedy" :
             result = 40000;
             if(aPerformance.audience > 30) {
@@ -47,7 +47,7 @@ function amountFor(aPerformance, play) {
 
             break;
         default:
-            throw new Error(`알 수 없는 장르 : ${play.type}`);
+            throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
 
     }
     return result;
