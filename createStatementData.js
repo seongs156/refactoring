bbacf07b4f4cdfe0e011f0a2d86d6eb7f14b1287
ksh <seongs156@gmail.com>
2022-11-24 14:16:45 +0900

    volumeCredits 오버라이딩

--- createStatementData.js
+++ createStatementData.js
@@ -18,31 +18,11 @@ class PerformanceCalculator {
     }
 
     get amount() {
-        let result = 0;
-        switch (this.play.type) {
-            case "tragedy" :
-                throw '오류발생';
-
-            case "comedy" :
-                result = 30000;
-                if(this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default :
-                throw new Error(`알 수 없는 장르 : ${this.play.type}`);
-
-        }
-        return result;
+        console.log('서브클래스에서 처리하도록 설계되었습니다.');
+        throw new Error('서브클래스에서 처리하도록 설계되었습니다.');
     }
     get volumeCredits() {
-        let result = 0;
-        result += Math.max(this.performance.audience - 30, 0);
-        if ("comedy" === this.play.type) {
-            result += Math.floor(this.performance.audience / 5);
-        }
-        return result;
+        return Math.max(this.performance.audience - 30, 0);
     }
 }
 
@@ -56,7 +36,17 @@ class TragedyCalculator extends PerformanceCalculator{
     }
 }
 class ComedyCalculator extends PerformanceCalculator{
-
+    get amount() {
+        let result = 30000;
+        if(this.performance.audience > 20) {
+            result += 1000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
+    }
 }
 
 export default function createStatementData(invoice, plays) {
