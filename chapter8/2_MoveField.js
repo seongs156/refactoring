6d07fece9e75dc52be8f41369d7e084a4578de58
ksh <seongs70@naver.com>
2023-05-01 11:35:32 +0900

    8.2 필드 옮기기

---
+++ chapter8/2_MoveField.js
@@ -0,0 +1,34 @@
+/******* 8.2 필드 옮기기 ********/
+/*******************************/
+
+class Customer {
+    constructor(name, discountRate) {
+        this._name = name;
+        this._contract = new CustomerContract(dateToday());
+        this._setDiscountRate(discountRate);
+    }
+
+    get discountRate() {
+        return this._contract.discountRate;
+    }
+    _setDiscountRate(aNumber) {
+        this._contract.discountRate = aNumber;
+    }
+
+    becomePreferred() {
+        this._setDiscountRate(this.discountRate += 0.03);
+    }
+    applyDiscount(amount) {
+        return amount.subtract(amount.multiply(this.discountRate));
+    }
+}
+
+class CustomerContract {
+    constructor(startDate, discountRate) {
+        this._startDate = startDate;
+        this._discountRate = discountRate;
+    }
+
+    get discountRate() { return this._discountRate; }
+    set discountRate(arg) { this._discountRate = arg; }
+}
