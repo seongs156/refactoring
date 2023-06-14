87be3707a93d67ec318de956e117de7babfe787a
ksh <seongs70@naver.com>
2023-06-14 15:24:01 +0900

    11.9 함수를 명령으로 바꾸기

---
+++ chapter11/9_ReplaceFunctionWithCommand.js
@@ -0,0 +1,36 @@
+/******* 11.9 함수를 명령으로 바꾸기 ********/
+/******************************************/
+
+function score(candidate, medicalExam, scoringGuide) {
+    return new Scorer(candidate,medicalExam, scoringGuide).execute();
+}
+
+class Scorer{
+    constructor(candidate,medicalExam, scoringGuide) {
+        this._candidate = candidate;
+        this._medicalExam = medicalExam;
+        this._scoringGuide = scoringGuide;
+    }
+    execute() {
+        this._result = 0;
+        this._healthLevel = 0;
+        this._highMedicalRiskFlag = false;
+
+        this.scoreSmoking();
+        this._certificationGrade = "regular";
+
+        if (this._scoringGuide.stateWithLowCertification(this._candidate.originState)) {
+            this._certificationGrade = "low";
+            this._result -= 5;
+        }
+        this._result -= Math.max(this._healthLevel - 5, 0);
+        return this._result;
+    }
+
+    scoreSmoking() {
+        if (this._medicalExam.isSmoker) {
+            this._healthLevel += 10;
+            this._highMedicalRiskFlag = true;
+        }
+    }
+}
